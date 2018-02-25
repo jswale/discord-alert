073616ca40be17beeb521460d32927ad9fd4443c
Jonathan Swale <jonathan.swale@gmail.com>
2018-02-25 14:45:14 +0100

    Add resume information of a pokedex entry

--- src/helpers/Utils.js
+++ src/helpers/Utils.js
@@ -9,13 +9,45 @@ function normalize(name) {
 }
 
 function getPokedexEntry(pokemon) {
-    let normalized = normalize(pokemon.name);
-    let entry = Pokedex[normalized];
+    let entry = getPokedexEntryByName(pokemon.name);
     if (entry === undefined) {
         Logger.warn(`Unable to find pokemon with name ${pokemon.name}`);
     }
     return entry;
 }
+
+function getPokedexEntryByName(name) {
+    return Pokedex[normalize(name)];
+}
+
+function getPokedexEntryByNumber(number) {
+    return Object.values(Pokedex).find(entry => entry.Number === number);
+}
+
+function getPokemonEvolutions(entry, current = false) {
+    let evo = getPokedexEntryByNumber(entry.Number);
+    let requirements = evo['Next Evolution Requirements'];
+    return (current ? `__${evo.NameLocale}__` : evo.NameLocale) + (requirements ? ` (${requirements.Amount})` : '');
+}
+
+function getPokedexEntryDetail(entry) {
+    let sheet = `**[${entry.Number}] ${entry.NameLocale}**`;
+    sheet += `\n* **Type(s)**: ${Object.keys(entry).filter(key => key.startsWith('Type ')).map(type => entry[type]).join(', ')}`;
+    sheet += `\n* **Fast Attack(s)**: ${entry["Fast Attack(s)"].join(', ')}`;
+    sheet += `\n* **Special Attack(s)**: ${entry["Special Attack(s)"].join(', ')}`;
+    sheet += `\n* **Attack**: ${entry.BaseAttack}`;
+    sheet += `\n* **Defense**: ${entry.BaseDefense}`;
+    sheet += `\n* **Stamina**: ${entry.BaseStamina}`;
+    sheet += `\n* **Buddy distance**: ${entry.BuddyDistanceNeeded}km`;
+
+    let evolutions = (entry['Previous evolution(s)'] || []).map(entry => getPokemonEvolutions(entry));
+    evolutions.push(getPokemonEvolutions(entry, true));
+    evolutions = evolutions.concat((entry['Next evolution(s)'] || []).map(entry => getPokemonEvolutions(entry)));
+    sheet += `\n* **Evolution(s)**: ${evolutions.join(' > ')}`;
+
+    return sheet;
+}
+
 
 function getRandomValue(list, defaultValue) {
     return list ? (Array.isArray(list) ? list[Math.floor(Math.random() * list.length)] : list) : defaultValue;
@@ -41,4 +73,4 @@ function getRangeInt(prop, min, max) {
     return Math.floor(Math.random() * max) + min;
 }
 
-module.exports = {normalize, getPokedexEntry, getRangeInt, getRandomValue};
+module.exports = {normalize, getPokedexEntry, getPokedexEntryDetail, getPokedexEntryByName, getPokedexEntryByNumber, getRangeInt, getRandomValue};
